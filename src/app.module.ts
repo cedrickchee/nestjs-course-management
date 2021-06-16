f74898609cf303c582f8533eb2fbcd7dd005650d
Cedric Chee <cedric@invictusbyte.com>
2021-06-16 16:13:12 +0800

    chore (infra): TypeORM MongoDB settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      // This flag is MongoDB specific. I don't really know what it does.
+      // All I know is that you need to set it true right now due to
+      // some deprecation that is about to happen in the future.
+      useUnifiedTopology: true,
+      entities: [],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
