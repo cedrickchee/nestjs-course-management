26f01e6f9fcf0d9eba663438ea37988e5cc1ea1e
Cedric Chee <cedric@invictusbyte.com>
2021-06-16 17:02:14 +0800

    feat: create lesson GraphQL mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -14,5 +17,11 @@ export class LessonResolver {
   }
 
   @Mutation((returns) => LessonType)
-  createLesson() {}
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
 }
